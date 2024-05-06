51e3d8f08e85f030305a8bd4b610cc6f9cc7f667
DevShaded <devshaded@devshaded.com>
2024-05-06 13:53:40 +0200

    refactor: login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,14 +1,26 @@
 import { signIn } from "@/auth"
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
 
 export default async function SignIn() {
     return (
-        <form
-            action={async () => {
-                "use server"
-                await signIn("github", { redirectTo: '/dashboard' })
-            }}
-        >
-            <button type="submit">Signin with GitHub</button>
-        </form>
+        <div className="flex min-h-full flex-1 flex-col items-center justify-center px-6 py-20 lg:px-8">
+            <Card className="w-full sm:max-w-md mt-6 px-6 py-4">
+                <CardHeader>
+                    <CardTitle>Login</CardTitle>
+                </CardHeader>
+                <CardContent>
+                    <p>Login with GitHub to continue</p>
+                </CardContent>
+                <CardFooter>
+                    <form action={async () => {
+                        "use server"
+                        await signIn("github", {redirectTo: '/dashboard'})
+                    }}>
+                        <Button type="submit">Sign in with GitHub</Button>
+                    </form>
+                </CardFooter>
+            </Card>
+        </div>
     )
 }
